ff2968e8ee1e2314acdd3e455f9f729aaf9efa65
Nguyen Tuan Anh <tuananhkl99@gmail.com>
2022-09-08 09:03:43 +0700

    1st commit: print 10 employees

---
+++ GitCommands/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Bogus;
+
+namespace GitCommands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new Faker<Employee>()
+                .RuleFor(x => x.Id, x => Guid.NewGuid())
+                .RuleFor(x => x.Name, x => x.Person.LastName)
+                .RuleFor(x => x.Age, x => 2022 - x.Person.DateOfBirth.Year)
+                .Generate(100);
+
+            var tenEmployees = employees.Take(10).ToList();
+
+            foreach (var employee in tenEmployees)
+            {
+                Console.WriteLine($"ID: {employee.Id}, Name: {employee.Name}, Age: {employee.Age}");
+            }
+        }
+    }
+}
